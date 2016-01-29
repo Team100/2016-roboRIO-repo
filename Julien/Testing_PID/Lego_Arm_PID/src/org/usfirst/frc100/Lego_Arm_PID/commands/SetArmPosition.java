994b2478fd39bf33b3e9f291ce5a946997ef13c5
Colin Chen <colin.a.chen@gmail.com>
2016-01-28 17:48:49 -0800

    Still attempting to close the loop

--- Julien/Testing_PID/Lego_Arm_PID/src/org/usfirst/frc100/Lego_Arm_PID/commands/SetArmPosition.java
+++ Julien/Testing_PID/Lego_Arm_PID/src/org/usfirst/frc100/Lego_Arm_PID/commands/SetArmPosition.java
@@ -8,6 +8,9 @@ package org.usfirst.frc100.Lego_Arm_PID.commands;
 
 import org.usfirst.frc100.Lego_Arm_PID.Robot;
 
+
+import org.usfirst.frc100.SlideWinder.SlideWinder;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -37,16 +40,19 @@ public class SetArmPosition extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    }
+		Robot.robotArm.updatePID();
+	}
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
-    }
+    	Robot.robotArm
+		return Robot.robotArm.isInPosition() || (!Robot.robotArm.isGoingUp() && Robot.robotArm.isAtLowLimit()) || (Robot.robotArm.isGoingUp() && Robot.robotArm.isAtHighLimit());
+	}
+
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.robotArm.updatePID();
+    	
     }
 
     // Called when another command which requires one or more of the same
