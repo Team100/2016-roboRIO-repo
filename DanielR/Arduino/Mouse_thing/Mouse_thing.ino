f91d8f7884ab2401359b5beed8b7da6e393def9d
danielroush <daroushall@gmail.com>
2016-01-31 10:17:39 -0800

    finished mouse

---
+++ DanielR/Arduino/Mouse_thing/Mouse_thing.ino
@@ -0,0 +1,45 @@
+#include "Joystick.h"
+#include "Keyboard.h"
+#include "Mouse.h"
+
+
+
+const int analogInPin1 = A1;
+const int buttonPin = 2;
+const int analogInPin0 = A0;
+
+int buttonState = 0;
+int sensorValue = 0;
+int buttonPressed;
+int i;
+int type;
+int ecks;
+int whyy;
+int xValue;
+int yValue;
+
+void setup() {
+  Joystick.begin();
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+}
+
+
+void loop() {
+  ecks = analogRead(analogInPin0);
+  whyy = analogRead(analogInPin1);
+  xValue = map(ecks, 0, 1023, -1, 1);
+  yValue = map(whyy, 0, 1023, -1, 1);
+
+  Joystick.setXAxis(xValue);
+  Joystick.setYAxis(yValue);
+  Mouse.move(xValue,0,0);
+  Mouse.move(0,yValue,0);
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    Mouse.press();
+  } else {
+    Mouse.release();
+  }
+  
+}
