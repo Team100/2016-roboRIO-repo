5bf740bb83b0f09c43e6d854ca3845a723eb3d67
Rodrigo <rmejia.g36@gmail.com>
2016-02-05 17:28:21 -0800

    My widget works dawgies......

--- Rodrigo/Widgets2016/src/team100/smartdashboard/extensions/CopyOfElevatorWidget.java
+++ Rodrigo/Widgets2016/src/team100/smartdashboard/extensions/CopyOfElevatorWidget.java
@@ -1,12 +1,10 @@
 <xml version="1.0">
 <dashboard>
 	<static-widget class="team100.smartdashboard.extensions.GRIPwidget">
-		<location x="11" y="63"/>
+		<location x="0" y="0"/>
 	</static-widget>
-	<widget field="X" type="String" class="edu.wpi.first.smartdashboard.gui.elements.TextBox">
-		<location x="17" y="20"/>
-	</widget>
 </dashboard>
 <live-window>
 </live-window>
+	<hidden field="ExmpleVar: "/>
 </xml>
--- Rodrigo/Widgets2016/src/team100/smartdashboard/extensions/GRIPwidget.java
+++ Rodrigo/Widgets2016/src/team100/smartdashboard/extensions/GRIPwidget.java
@@ -19,45 +19,53 @@ public class GRIPwidget extends StaticWidget {
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "GRIP Widget";
 	public static final DataType[] TYPES = { DataType.NUMBER };
-	private final NetworkTable mCR = NetworkTable.getTable("GRIP/myContoursReport");
+	public final NetworkTable mCR = NetworkTable
+			.getTable("GRIP/myContoursReport");
 	private final JPanel p = new JPanel();
-    private final Color team100orange = new Color(0xF4, 0x92, 0x07);
-    private final JTextPane x = new JTextPane();
-    private GridLayout g;
+	private final Color team100orange = new Color(0xF4, 0x92, 0x07);
+	private final JTextPane x = new JTextPane();
+	Font font = new Font("TimesNewRoman", Font.BOLD, 80);
+	public GridLayout g;
 
 	public void propertyChanged(Property arg0) {
-		//Empty
+		// Empty
 	}
-	
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setColor(team100orange);
-        g.fillRect(0, 0, getWidth(), getHeight());
-    }
-	
+
+	@SuppressWarnings("deprecation")
+	public void paintComponent(final Graphics g) {
+		super.paintComponent(g);
+		g.setColor(team100orange);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.setColor(Color.black);
+		g.fillRect(80, 160, 640, 480);
+		for (int i = 0; i < mCR.getNumberArray("centerX").length; i++) {
+			g.fillRect((int) (mCR.getNumberArray("centerX")[i] + 80),
+					(int) (mCR.getNumberArray("centerY")[i] + 160), 1, 1);
+			g.setColor(Color.white);
+		}
+	}
+
 	public void init() {
-		setPreferredSize(new Dimension(500,  500));
+		setPreferredSize(new Dimension(800, 800));
 		setLayout(new FlowLayout());
-        g = new GridLayout(3, 5);
-        g.setHgap(5);
-        g.setVgap(5);
-        x.setText("GRIP Widget");
+		g = new GridLayout(3, 5);
+		g.setHgap(5);
+		g.setVgap(5);
+		x.setText("GRIP Widget");
+		x.setFont(font);
+		x.setForeground(Color.white);
+		x.setBackground(team100orange);
 		p.setSize(500, 500);
-        p.setLayout(g);
-        p.setBackground(team100orange);
-        add(p);
-        add(x);
-        
-		mCR.addTableListener(new ITableListener(){
-			
-			public void valueChanged(ITable source, String key, Object value, boolean isNew) {
-				if(value instanceof double[]){
-					double[] a = (double[]) value;
-				for(int i = 0; i < a.length; i++){
-					Double val = new Double(a[i]);
-				System.out.println(key  + "[" + i + "]" + val.toString());
-				}
-				}
+		p.setLayout(g);
+		p.setBackground(team100orange);
+		add(p);
+		add(x);
+
+		mCR.addTableListener(new ITableListener() {
+
+			public void valueChanged(ITable source, String key, Object value,
+					boolean isNew) {
+				repaint();
 			}
 		});
 	}
