1f37dea568a1d27126580f601acedb0156d1d1d3
Rodrigo <rmejia.g36@gmail.com>
2016-02-09 16:43:39 -0800

    Revert "..."
    This reverts commit 011c150a7ac214237e0bd2f31f42b11ebee1be2c.

---
+++ Rodrigo/Widgets2016/src/team100/smartdashboard/extensions/CopyOfElevatorWidget.java
@@ -0,0 +1,10 @@
+<xml version="1.0">
+<dashboard>
+	<static-widget class="edu.wpi.grip.smartdashboard.GRIPExtension">
+		<location x="-1" y="3"/>
+		<property name="GRIP Address" value="10.1.0.2"/>
+	</static-widget>
+</dashboard>
+<live-window>
+</live-window>
+</xml>
