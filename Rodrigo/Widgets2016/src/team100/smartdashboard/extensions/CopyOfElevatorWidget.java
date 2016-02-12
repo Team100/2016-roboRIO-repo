85a2cbaae46f25295117bae75c423a6d6899e09c
Rodrigo <rmejia.g36@gmail.com>
2016-02-11 16:53:19 -0800

    Done

---
+++ Rodrigo/Widgets2016/src/team100/smartdashboard/extensions/CopyOfElevatorWidget.java
@@ -0,0 +1,9 @@
+<xml version="1.0">
+<dashboard>
+	<static-widget class="edu.wpi.grip.smartdashboard.GRIPExtension">
+		<location x="0" y="0"/>
+	</static-widget>
+</dashboard>
+<live-window>
+</live-window>
+</xml>
