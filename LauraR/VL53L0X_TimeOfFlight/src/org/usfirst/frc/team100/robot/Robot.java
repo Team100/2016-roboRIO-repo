3abdda79ee81f7829939752584374f789aa4248c
LauraRhodes <laura@pyxiseng.com>
2017-03-07 13:32:28 -0800

    Corrected indentation

--- LauraR/VL53L0X_TimeOfFlight/src/org/usfirst/frc/team100/robot/Robot.java
+++ LauraR/VL53L0X_TimeOfFlight/src/org/usfirst/frc/team100/robot/Robot.java
@@ -2,7 +2,6 @@
 package org.usfirst.frc.team100.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
