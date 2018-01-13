2b7396afa72042e04b155fddd6435ae1fc4bbc2c
alexanderbeaver799710 <31376007+alexanderbeaver799710@users.noreply.github.com>
2018-01-13 11:02:26 -0800

    Network Table fake data generator- part 1
    As of this point, I have made a basic network table data generator. It simulates motion every 20 ms with the purpose of testing shuffleboard.
    
    Here is the full list of what has changed:
    
    - Create new Python project
    - Add basic testing code for network tables
    - Allow for configuration in config.py
    - Executable from CMD or from PyCharm

---
+++ AlexB/FRCNetworkTables/main.py
@@ -0,0 +1,38 @@
+## Check out the documentation at: http://robotpy.readthedocs.io/en/stable/guide/nt.html#networktables-guide
+
+from networktables import NetworkTables #Installed with pip
+import time
+import random
+import config
+# To see messages from networktables, you must setup logging
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+print("STARTED")
+NetworkTables.initialize(server=config.ip)
+
+print("Allocating time to connect")
+time.sleep(5)
+table = NetworkTables.getTable('SmartDashboard')
+print("GOT TABLE")
+
+
+# This retrieves a boolean at /SmartDashboard/foo
+foo = table.getBoolean('foo', True)
+
+table.putBoolean('bar',False)
+print("PUT VALUE IN NETWORKTABLE")
+def generateRandomValues():
+    table.putNumber('Int1',random.randint(1,1000))
+    table.putNumber('Int2', random.randint(1, 1000))
+    testBool = random.randint(0,1)
+    if(testBool == 1):
+        table.putBoolean('myBool',True)
+    else:
+        table.putBoolean('myBool',False)
+    time.sleep(0.02)  #Wait 20 milliseconds
+
+runLoop = True
+print("Loop Started")
+while(runLoop):
+    generateRandomValues()  # Call my function
