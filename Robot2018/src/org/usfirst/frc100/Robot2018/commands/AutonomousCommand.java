95b7a530cd2323bba2be3d3b92c6ab097bf63741
SethReis <795091@seq.org>
2018-01-28 09:57:55 -0800

    DriveTrain Changes: UNDONE

--- Robot2018/src/org/usfirst/frc100/Robot2018/commands/AutonomousCommand.java
+++ Robot2018/src/org/usfirst/frc100/Robot2018/commands/AutonomousCommand.java
@@ -12,6 +12,9 @@
 package org.usfirst.frc100.Robot2018.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc100.Robot2018.Robot;
+import org.usfirst.frc100.Robot2018.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 /**
  *
@@ -42,6 +45,7 @@ public class AutonomousCommand extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
