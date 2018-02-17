d7b9a8f42eb201b66048c096333a76c2cb527414
alexanderbeaver799710 <31376007+alexanderbeaver799710@users.noreply.github.com>
2018-02-17 14:02:31 -0800

    Jaci Turn To
    This has not been tested yet. It uses turning with Jaci's Pathfinding

--- Robot2018/src/org/usfirst/frc100/Robot2018/commands/TurnTo.java
+++ Robot2018/src/org/usfirst/frc100/Robot2018/commands/TurnTo.java
@@ -1,23 +1,37 @@
 package org.usfirst.frc100.Robot2018.commands;
 
+import org.usfirst.frc100.Robot2018.subsystems.DriveTrain;
+
 import edu.wpi.first.wpilibj.command.Command;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Waypoint;
 
 /**
  *
  */
 public class TurnTo extends Command {
-
-    public TurnTo() {
+	private static float degreeOffset;
+	private static float currentHead;
+	private static String direction;
+    public TurnTo(float degreesToTurn, String directionToTurn) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	degreeOffset = degreesToTurn;
+    	direction = directionToTurn;
+    	
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Waypoint[] points = new Waypoint[] {
+    			new Waypoint(0,0,Pathfinder.d2r(degreeOffset)),
+    	};
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	currentHead = DriveTrain.navxCompassHeading;
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
