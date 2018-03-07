27af56d6562de63cb771094269375b9f908db24d
GDW1 <guydwilks@gmail.com>
2018-03-06 16:41:20 -0800

    COMM

---
+++ Robot2018/src/org/usfirst/frc100/Robot2018/commands/passTime.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc100.Robot2018.commands;
+
+import org.usfirst.frc100.Robot2018.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class passTime extends Command {
+	public static double timePassed;
+	private boolean done;
+    public passTime() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	//taking the time in nanoseconds and  converting to milliseconds 
+    	timePassed = System.nanoTime()/1000000;
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if(System.nanoTime() ==  timePassed + 1000*(10)) {
+    		done = true;
+    	}
+    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return done;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ Robot2018/src/org/usfirst/frc100/Robot2018/subsystems/DoubleSolen.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc100.Robot2018.subsystems;
+
+public class DoubleSolen {
+
+}
