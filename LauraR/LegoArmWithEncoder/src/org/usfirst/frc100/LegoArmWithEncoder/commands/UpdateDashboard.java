a4b797487d157402c165514aa8cdc9f2a94fd5c2
LauraRhodes <laura@pyxiseng.com>
2018-07-25 15:00:30 -0700

    Modified Lego Arm Demo for inclusion of CUI shaft encoder.

---
+++ LauraR/LegoArmWithEncoder/src/org/usfirst/frc100/LegoArmWithEncoder/commands/UpdateDashboard.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc100.LegoArmWithEncoder.commands;
+
+import org.usfirst.frc100.LegoArmWithEncoder.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class UpdateDashboard extends Command {
+
+    public UpdateDashboard() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	SmartDashboard.putString("RobotArm/.type", "SubSystem");
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.robotArm.updateDashboard();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false; // NEVER FINISH!
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
